4112562eea75ffe2e201c54aeca25d82afc59665
Dhika <putraadhika15@gmail.com>
2024-05-21 07:56:43 +0700

    revision migration and seeder data

---
+++ migrations/20240521003246-create-payment.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      order_id: {
+        type: Sequelize.STRING
+      },
+      amount: {
+        type: Sequelize.FLOAT(8,2)
+      },
+      timestamp: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Payments');
+  }
+};
---
+++ seeders/20240521004834-demo-payment.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Payments', [
+      {
+        order_id: 'pay-01',
+        amount: '10',
+        timestamp: new Date(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Payments', null, {});
+  }
+};
