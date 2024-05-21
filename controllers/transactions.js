f1d5072e2bba307adb30b1a8490a813b1d87e528
Dhika <putraadhika15@gmail.com>
2024-05-21 13:59:51 +0700

    add method update and get data payments

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,6 +2,7 @@
 const dbTransaction = require('../models/transaction/index')
 const transaction = dbTransaction.payment;
 
+// CREATE
 exports.add = (req, res) => {
     const post = {
         order_id: req.body.order_id,
@@ -18,5 +19,45 @@ exports.add = (req, res) => {
                 message: err
             })
         })
+    })
+}
+
+// GET
+exports.get = (req, res) => {
+    transaction.findAll({
+        attributes: [
+            'order_id','amount'
+        ]
+    }).then((data) => {
+        res.status(200).send({
+            data: data,
+            message: 'Data payments'
+        })
+    });
+}
+
+// UPDATE
+exports.update = async (req, res) => {
+    const put = {
+        id: req.params.id,
+        order_id: req.body.order_id,
+        amount: req.body.amount
+    }
+    
+    transaction.update({
+        order_id: put.order_id,
+        amount: put.amount
+    }, {
+        where: {
+            id: put.id
+        }
+    }).then((data) => {
+        res.status(200).json({
+            status: 'success'
+        })
+    }).catch((err) => {
+        res.status(500).json({
+        message: err
+    })
     })
 }
--- routes/transactions.js
+++ routes/transactions.js
@@ -3,4 +3,6 @@
 module.exports = function (app) {
     var transaction = require('../controllers/transactions');
     app.route('/transaction').post(transaction.add);
+    app.route('/get-transaction').get(transaction.get);
+    app.route('/update-transaction/:id').put(transaction.update);
 }
