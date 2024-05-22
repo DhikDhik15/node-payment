7e0cc2dd4285a785dbcf8a38cbb78bd69ec54e61
Dhika <putraadhika15@gmail.com>
2024-05-22 08:28:22 +0700

    add cors permission in backend

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser')
+const cors = require("cors");
 dotenv.config();
 
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
